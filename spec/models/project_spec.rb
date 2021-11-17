51d41597eebd7c2df77732e4b4f9996412aadab0
Nick S <52386340+LenceOfTheOrder@users.noreply.github.com>
2021-11-17 07:25:45 +0000

    "Create functional test for Projects controller"

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -33,3 +33,21 @@ RSpec.describe Project, type: :model do
 
   end
 end
+
+RSpec.describe ProjectsController, type: :controller do
+    context "GET #index" do
+      it "returns a success response" do
+        get :index
+        # expect(response.success).to eq(true)
+        expect(response).to be_success
+      end
+    end
+  
+    context "GET #show" do
+      let!(:project) { Project.create(title: "Test title", description: "Test description") }
+      it "returns a success response" do
+        get :show, params: { id: project }
+        expect(response).to be_success
+      end
+    end
+  end
